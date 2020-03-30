bc9ddf8e9c850a74c615fe3ddfd3b1088cb1649b
Rahul Singh <rahul.singh4800@gmail.com>
2020-03-30 22:20:49 +0530

    code added

---
+++ String/split_join.py
@@ -0,0 +1,11 @@
+"""
+In Python, a string can be split on a delimiter.
+"""
+
+def split_and_join(line):
+    return "-".join(line.split(" "))
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
