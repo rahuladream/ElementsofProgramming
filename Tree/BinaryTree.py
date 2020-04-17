825ab8b5cb6444cd73c5c19eb0d9551c1a47e7bb
Rahul Singh <rahul.singh4800@gmail.com>
2020-04-18 00:58:52 +0530

    tree part added

---
+++ Tree/BinaryTree.py
@@ -0,0 +1,48 @@
+"""
+
+>> One node is marked as root node
+>> every node other than the root is associated with the one parent node
+>> Each node can have an arbitrary number of child node
+
+"""
+
+class Node:
+    
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        # Compare the new value with the parent node
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    #Print the tree
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data),
+        if self.right:
+            self.right.PrintTree()
+
+
+
+root = Node(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+
+root.PrintTree()
