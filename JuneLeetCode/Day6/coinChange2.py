fe92e5bda6659db41736878a4ec82c66bac075c5
Rahul Singh <rahul.singh4800@gmail.com>
2020-06-07 22:37:55 +0530

    day6 june leetcode result

--- JuneLeetCode/Day6/coinChange2.py
+++ JuneLeetCode/Day6/coinChange2.py
@@ -1,11 +1,18 @@
 class Solution:
     def change(self, amount, coins):
+        tab = [0 for k in range(amount+1)]
 
-        pass
+        tab[0] = 1
+
+        for i in range(0, len(coins)):
+            for j in range(coins[i], amount+1):
+                tab[j] += tab[j - coins[i]]
+        return tab[amount]
+
 
 
 
 
 if __name__ == "__main__":
     a = Solution()
-    print(a.change(amount = 3, coins = [2]))
+    print(a.change(amount = 5, coins = [1, 2, 5]))
