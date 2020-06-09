8e0d78cb3553ec53b39a1f7f0ef12d7f8f65323d
Rahul Singh <rahul.singh4800@gmail.com>
2020-06-09 11:31:02 +0530

    day7 june leetcode

---
+++ JuneLeetCode/Day7/powerTwo.py
@@ -0,0 +1,16 @@
+import math
+class Solution:
+    def isPowerOfTwo(self, n):
+        
+        if n == 0:
+            # return if value is 0 
+            return False
+        
+        # function of check log base 2 i.e if the power of 2
+        return (math.ceil(math.log10(n) / math.log10(2)) == math.floor((math.log10(n) / math.log10(2))))
+
+
+
+if __name__ == "__main__":
+    a = Solution()
+    print(a.isPowerOfTwo(2))        
