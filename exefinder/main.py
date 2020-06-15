6cb676e97947ee3a1f6a31e351340e9f714f837f
Rahul Singh <rahul.singh4800@gmail.com>
2020-06-15 23:31:48 +0530

    exe file finder

---
+++ exefinder/main.py
@@ -0,0 +1,17 @@
+import os, fnmatch
+# to get path and matching the excat pattern
+
+def find(pattern, path):
+    res = []
+    # store the result extracted from folder
+    for root, dirs, files in os.walk(path):
+        # import pdb; pdb.set_trace()
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                # checking the .exe pattern if it match or not
+                res.append(os.path.join(root, name))
+                # if match return the path and folder name?
+        return res
+
+
+print(find('*.exe', 'folder'))
